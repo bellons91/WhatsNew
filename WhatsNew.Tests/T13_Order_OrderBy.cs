ee19d713962a72ac6c28427c58e662519d2d6a9c
Davide Bellone <davide.bellone>
2022-11-25 11:16:22 +0100

    Added tests

---
+++ WhatsNew.Tests/T13_Order_OrderBy.cs
@@ -0,0 +1,43 @@
+﻿namespace WhatsNew.Tests
+{
+    public class T13_Order_OrderBy
+    {
+        public static List<User> _users = new List<User> {
+            new User(35, "Foo"),
+            new User(11, "Bar"),
+            new User(64, "Baz"),
+            };
+
+        [Test]
+        public void order_by_field()
+        {
+            var sortedUsers = _users.OrderBy(x => x.Id);
+
+            Assert.That(sortedUsers.First().Id, Is.EqualTo(11));
+            Assert.That(sortedUsers.Last().Id, Is.EqualTo(64));
+        }
+
+        [Test]
+        public void order_by_defaul()
+        {
+            List<OrderedUser> users = _users.Select(u => new OrderedUser { Id = u.Id, Name = u.Name }).ToList();
+
+            var sortedUsers = users.Order();
+            Assert.That(sortedUsers.First().Id, Is.EqualTo(11));
+            Assert.That(sortedUsers.Last().Id, Is.EqualTo(64));
+        }
+
+        public class OrderedUser : IComparable<OrderedUser>
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public int CompareTo(OrderedUser? other)
+            {
+                return Id.CompareTo(other.Id);
+            }
+        }
+
+        public record User(int Id, string Name);
+    }
+}
---
+++ WhatsNew.Tests/T14_Dictionary_syntax.cs
@@ -0,0 +1,29 @@
+﻿namespace WhatsNew.Tests
+{
+    public class T14_Dictionary_syntax
+    {
+        [Test]
+        public void old_syntax()
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "uno");
+            dict.Add(2, "due");
+            dict.Add(3, "tre");
+
+            Assert.That(dict.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void new_syntax()
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>()
+            {
+                [1] = "uno",
+                [2] = "due",
+                [3] = "tre"
+            };
+
+            Assert.That(dict.Count, Is.EqualTo(3));
+        }
+    }
+}
