d7fbaa885624cb79b2a3bd413bb24e705ef7c78f
Davide Bellone <davide.bellone>
2023-02-08 14:37:25 +0100

    Added examples to Pattern matchin

--- WhatsNew.Tests/T15_Pattern_Matching.cs
+++ WhatsNew.Tests/T15_Pattern_Matching.cs
@@ -44,7 +44,7 @@
 
         [Test]
         [DotNet7]
-        public void can_use_list_patterns_with_underscore()
+        public void can_use_list_patterns_with_discard()
         {
             string SelfOrMessage(int[] s)
             {
@@ -53,6 +53,38 @@
             }
 
             Assert.That(SelfOrMessage(new int[] { 1, 2, 3 }), Is.EqualTo("CIAO"));
+            Assert.That(SelfOrMessage(new int[] { 1, 6, 2, 3 }), Is.EqualTo("error!"));
+            Assert.That(SelfOrMessage(new int[] { 6, 3, 8, 4 }), Is.EqualTo("error!"));
+        }
+
+        [Test]
+        [DotNet7]
+        public void can_use_list_patterns_with_var()
+        {
+            string SelfOrMessage(int[] s)
+            {
+                if (s is [_, 2, int third]) return "CIAO_" + third;
+                return "error!";
+            }
+
+            Assert.That(SelfOrMessage(new int[] { 1, 2, 3 }), Is.EqualTo("CIAO_3"));
+            Assert.That(SelfOrMessage(new int[] { 1, 6, 2, 3 }), Is.EqualTo("error!"));
+            Assert.That(SelfOrMessage(new int[] { 6, 3, 8, 4 }), Is.EqualTo("error!"));
+        }
+
+        [Test]
+        [DotNet7]
+        public void can_use_list_patterns_with_condition()
+        {
+            string SelfOrMessage(int[] s)
+            {
+                if (s is [0 or 1, > 2, int third]) return "CIAO_" + third;
+                return "error!";
+            }
+
+            Assert.That(SelfOrMessage(new int[] { 0, 4, 3 }), Is.EqualTo("CIAO_3"));
+            Assert.That(SelfOrMessage(new int[] { 6, 4, 3 }), Is.EqualTo("error!"));
+            Assert.That(SelfOrMessage(new int[] { 1, 2, 3 }), Is.EqualTo("error!"));
             Assert.That(SelfOrMessage(new int[] { 1, 6, 2, 3 }), Is.EqualTo("error!"));
             Assert.That(SelfOrMessage(new int[] { 6, 3, 8, 4 }), Is.EqualTo("error!"));
         }
