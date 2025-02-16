785b453baa3b3cceb631ce3c6c06374ec733a7f8
Davide Bellone <davide.bellone@outlook.com>
2025-02-16 22:16:45 +0100

    Added examples about params keyword

---
+++ WhatsNew.Tests/About_ParamsKeyword.cs
@@ -0,0 +1,43 @@
+﻿namespace WhatsNew.Tests
+{
+    public class About_ParamsKeyword
+	{
+
+        [Test]
+        [DotNetCore3]
+        public void Params_WorksWithArray()
+        {
+			int SumAll(params int[] nums) => nums.Sum();
+
+            Assert.That(SumAll(1,2,3), Is.EqualTo(6));
+        }
+
+        [Test]
+        [DotNet9]
+        public void Params_WorksWithIEnumerable()
+        {
+			int SumAll(params IEnumerable<int> nums) => nums.Sum();
+
+            Assert.That(SumAll(1, 2, 3), Is.EqualTo(6));
+        }
+
+        [Test]
+        [DotNet9]
+        public void Params_WorksWithHashSet()
+        {
+            int SumAll(params HashSet<int> nums) => nums.Sum();
+
+            Assert.That(SumAll(1, 2, 3), Is.EqualTo(6));
+        }
+
+        [Test]
+        [DotNet9]
+        public void Params_WorksWithList()
+        {
+            int SumAll(params List<int> nums) => nums.Sum();
+
+            Assert.That(SumAll(1, 2, 3), Is.EqualTo(6));
+        }
+
+    }
+}
