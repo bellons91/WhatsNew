9c81d6a05de452baba8691a7117d8aa80c51aa66
Davide Bellone <davide.bellone@outlook.com>
2025-02-16 22:04:16 +0100

    Add project with old .NET but new C#

---
+++ OddTargetingClassLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace OddTargetingClassLibrary
+{
+    public class Class1
+    {
+        List<string> a = new List<string>();
+        List<string> b = new();
+        List<string> c = ["foo"];
+    }
+}
